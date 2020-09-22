623fe51fa19805b35460e53add819306fd9fe817
asd153866714 <asd153866714@gmail.com>
2020-09-22 08:44:20 +0800

    test

--- Problem Solving/Approach/02.js
+++ Problem Solving/Approach/02.js
@@ -1,1 +1,21 @@
 // Explore Concrete Examples
+
+// Write a function which takes in a string and returns counts of each char in the string
+charCount(str)
+
+// Simple Examples
+str = 'aaa'     // {a: 4}
+str = 'hello'   // {h: 1, e: 1, l: 2, o: 1}
+
+// More Complex Examples
+str = 'my phone number is 19093'
+str = 'Hello Hi'
+
+// Empty Inputs
+str = ''
+
+// Invalid Inputs (Edge Case)
+str = 123
+str = true
+str = {a: 123}
+str = null
--- Problem Solving/Approach/05.js
+++ Problem Solving/Approach/05.js
@@ -1,1 +1,36 @@
 // Look Back and Refactor
+
+function charCount1(str) {
+    let obj = {}
+    for (let char of str) {
+        char = char.toLowerCase()
+        if (/[a-z0-9]/.test(char)) {
+            obj[char] = ++obj[char] || 1
+        }
+        // obj[char] = /[a-z0-9]/.test(char) ? ++obj[char] : 1
+    }
+    return obj
+}
+
+function charCount2(str) {
+    let obj = {}
+    for (let char of str) {
+        if (isAlphaNumeric(char)) {
+            char = char.toLowerCase()
+            obj[char] = ++obj[char] || 1
+        }
+    }
+    return obj
+}
+
+function isAlphaNumeric(char) {
+    let code = char.charCodeAt(0)
+    if (!(code > 47 && code < 58) && 
+        !(code > 64 && code < 91) &&
+        !(code > 96 && code < 123)) {
+            return false
+        }
+    return true
+}
+
+console.log(charCount2('Hello'))
